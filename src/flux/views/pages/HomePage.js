c41cffb2a01b1284d7e1c31a5ac2ab461be8f248
gocreating <gocreating@gmail.com>
2016-03-06 03:28:08 +0800

    Edit homepage

--- src/flux/views/pages/HomePage.js
+++ src/flux/views/pages/HomePage.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 export default class HomePage extends React.Component {
-  handleClick() {
-    alert('It works');
-  }
-
   render() {
     return <div className="container">
-      <h1>Home</h1>
-      <button onClick={this.handleClick}>Click me</button>
+      <h1>Profile Card</h1>
+      <p>
+        This is the demo site for project
+        <a href="https://github.com/gocreating/pcard"> pcard</a>
+      </p>
     </div>;
   }
 };
