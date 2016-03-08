9f98e7895597445c310cf28f35b450947ca10c37
gocreating <gocreating@gmail.com>
2016-03-09 03:10:34 +0800

    Improve component `Field`

--- src/flux/views/components/Field.js
+++ src/flux/views/components/Field.js
@@ -1,43 +1,70 @@
 import React from 'react';
 import objectAssign from 'object-assign';
+import FieldTypes from '../../constants/FieldTypes';
 import ProfileStore from '../../stores/ProfileStore';
 import * as fieldComponents from './fieldComponents';
 
 export default class Field extends React.Component {
-  _getInfo() {
-    const { fieldDefinitionMap } = ProfileStore.getState();
-    const fieldDefinition = fieldDefinitionMap[this.props.path];
-    const { type, label } = fieldDefinition;
+  constructor(props) {
+    super(props);
 
-    let initValue;
-    if (type.defaultValue instanceof Object) {
-      initValue = objectAssign({},
-        type.defaultValue,
-        fieldDefinition.value);
-    } else {
-      initValue = fieldDefinition.value || type.defaultValue;
+    let initValue = [];
+    let type, label, isArray;
+    try {
+      const { fieldDefinitionMap } = ProfileStore.getState();
+      const fieldDefinition = fieldDefinitionMap[this.props.path];
+      type = fieldDefinition.type;
+      label = fieldDefinition.label;
+      isArray = fieldDefinition.isArray;
+
+      if (!fieldDefinition.isArray) {
+        initValue = [
+          this._getComposedValue(type.defaultValue, fieldDefinition.value),
+        ];
+      } else {
+        initValue = fieldDefinition.value.map((value) =>
+          this._getComposedValue(type.defaultValue, value));
+      }
+    } catch (e) {
+      valueArr = [FieldTypes.Unknown.defaultValue];
     }
 
-    return {
-      type: type,
-      label: label,
-      initValue: initValue,
+    this.state = {
+      type,
+      label,
+      isArray,
+      initValue,
     };
   }
 
+  _getComposedValue(defaultValue, assignValue) {
+    let value;
+    if (defaultValue instanceof Object) {
+      // object
+      value = objectAssign({},
+        defaultValue,
+        assignValue);
+    } else {
+      // string
+      value = assignValue || defaultValue;
+    }
+    return value;
+  }
+
   getValue() {
-    return this.refs.fieldComponent.getValue();
+    const valueArr = this.state.initValue.map((value, idx) =>
+      this.refs[`fieldComponent${idx}`].getValue());
+    return this.state.isArray? valueArr: valueArr[0];
+  }
+
+  getInitValue() {
+    const valueArr = this.state.initValue;
+    return this.state.isArray? valueArr: valueArr[0];
   }
 
   isDirty() {
     const currentValue = this.getValue();
-    const {
-      type,
-      label,
-      initValue,
-    } = this._getInfo();
-
-    // use stringigy to implement deep equal comparison
+    const initValue = this.getInitValue();
     return JSON.stringify({v: currentValue}) !== JSON.stringify({v: initValue});
   }
 
@@ -45,17 +72,32 @@ export default class Field extends React.Component {
     const {
       type,
       label,
+      isArray,
       initValue,
-    } = this._getInfo();
+    } = this.state;
 
     const FieldComponent = fieldComponents[type.id];
 
+    // error control for unknown fieldtype
+    if (type.id === FieldTypes.Unknown.id || FieldComponent === undefined) {
+      return (
+        <div style={{color: 'red'}}>
+          Unknow Field: {this.props.path}
+        </div>
+      );
+    }
+
     return (
-      <FieldComponent
-        ref="fieldComponent"
-        label={label}
-        value={initValue}
-        {...this.props} />
+      <div>
+        {initValue.map((value, idx) =>
+          <FieldComponent
+            key={idx}
+            ref={`fieldComponent${idx}`}
+            label={label}
+            isShowLabel={idx === 0}
+            value={value}
+            {...this.props} />)}
+      </div>
     );
   }
 };
