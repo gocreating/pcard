89d7756872ff7bf9f1927c01f8ed5f491a80df63
gocreating <gocreating@gmail.com>
2016-03-11 03:39:20 +0800

    Handle bad request

--- src/server/utils/error.js
+++ src/server/utils/error.js
@@ -12,8 +12,13 @@ export default (errorType, res, next) => {
       };
       console.log('====================');
       if (errorType === Types.Db) {
+        res.status(400);
         if (err.errors !== undefined) {
-          errorResponse.errors.concat(err.errors);
+          errorResponse.errors = [{
+            name: err.name,
+            message: err.message,
+            errors: err.errors,
+          },];
         } else {
           errorResponse.errors.push(err);
         }
