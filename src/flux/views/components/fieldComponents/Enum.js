e25842209f2fcb0c6fc538f1ce338e1e998c8736
gocreating <gocreating@gmail.com>
2016-03-14 01:31:09 +0800

    Upgrade `enum` fieldtype

--- src/flux/views/components/fieldComponents/Enum.js
+++ src/flux/views/components/fieldComponents/Enum.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class String extends React.Component {
   getValue() {
+    if (this.props.isMultiple) {
+      const node = ReactDOM.findDOMNode(this.refs.value);
+      const options = [].slice.call(node.querySelectorAll('option'));
+      const selected = options
+        .filter((option) => option.selected);
+      const selectedValues = selected
+        .map((option) => ({
+          value: option.value,
+        }));
+      return selectedValues;
+    }
     return {
       value: this.refs.value.value,
     };
   }
 
   render() {
+    let defaultValue;
+    if (this.props.isMultiple &&
+        this.props.value instanceof Array) {
+      defaultValue = this.props.value.map((v) => v.value);
+    } else {
+      defaultValue = this.props.value;
+    }
     return (
       <div className="form-group">
         <label className="col-sm-2 control-label">
@@ -17,7 +36,8 @@ export default class String extends React.Component {
           <select
             className="form-control"
             ref="value"
-            defaultValue={this.props.value.value}>
+            multiple={this.props.isMultiple}
+            defaultValue={defaultValue}>
             {this.props.enum.map((e) =>
               <option
                 key={e.value}
